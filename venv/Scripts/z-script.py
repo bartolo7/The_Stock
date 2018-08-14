03e9c520f18dfdaf14554edadd4b9724493fbafb
unknown <davbr@kth.se>
2018-08-15 01:06:20 +0200

    zappa package

---
+++ venv/Scripts/z-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\Bardav\PycharmProjects\The_Stock\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'zappa==0.46.2','console_scripts','z'
+__requires__ = 'zappa==0.46.2'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('zappa==0.46.2', 'console_scripts', 'z')()
+    )
